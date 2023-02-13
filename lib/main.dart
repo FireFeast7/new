69bfaaa1bb1448761d8a63f6f19861175b5aaa7e
Ameya10 <ameyasurana10@gmail.com>
2023-02-13 09:32:09 +0530

    Commit 1

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "SnackBar",
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SnackBar'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Get.snackbar(
+                    "SnackBar Title",
+                    "This will be the SncakBar Message",
+                    showProgressIndicator: true,
+                    snackPosition: SnackPosition.BOTTOM,
+                  );
+                },
+                child: const Text(
+                  "Show SnackBar",
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
